df2230191cd26d7d539fc2b89792fc7ee87e3657
Gail Gong <ggong@ebony.local>
2017-03-14 15:27:43 -0700

    I deleted score_statistics functions in R and accompanying functions in man

--- R/regression_example_1_fn.R
+++
@@ -1,7 +0,0 @@
-regression_example_1_fn = function(e1, Lambda1, Lambda1_den){
-  U1 = sum(e1 - Lambda1)
-  V1 = sum(Lambda1_den)
-  example_1 = U1^2 / V1
-  p1 = pchisq(example_1 , df = 1, lower.tail = FALSE)
-  list(T = data.frame(T = example_1 , p = p1))
-}
