883ded366f0688629e628b11f16be07c2c281c8d
Gail Gong <gailgongster@gmail.com>
2017-06-22 15:45:41 -0700

    rmap.R This is the new function that will rule them all. I removed many functions and put them into inst/2017-06-22. Only those which support rmap or generate data remain.

---
+++ R/rmap.R
@@ -0,0 +1,46 @@
+rmap = function(e, t, r, tStar, design, riskGroup, rSummary, bootstrap){
+  baseArgs = baseArgsFn(e, t, r, tStar, design, riskGroup, rSummary, bootstrap, multicore = FALSE, verbose = FALSE) 
+  
+  if( baseArgs$ungrouped ) {
+    rvu = riskValidateUngroupedInternalFn(baseArgs)
+    dimnames(rvu$PNN$est) = list(NULL, c("assigned_risk", "observed_risk"))
+    dimnames(rvu$PNN$confBand) = list(NULL, c("lower", "upper"))
+    df_for_risk_plot = data.frame(100 * cbind(rvu$PNN$est, rvu$PNN$confBand))
+    risk_plot = ggplot(df_for_risk_plot, aes(x = assigned_risk, y = observed_risk, ymin = lower, ymax = upper)) +
+      geom_line() +
+      geom_ribbon(alpha = 0.3) +
+      geom_abline(slope = 1, intercept = 0, color = "red", linetype = 2) +
+      xlim(0,100) + ylim(0, 100) +
+      xlab("Assigned Risks (%)") + ylab("Observed Risks (%)")
+    
+    list(df_for_risk_plot = df_for_risk_plot,
+         risk_plot = risk_plot)
+  } else{
+    rv = if(baseArgs$sampling == "target_and_cohort_categories_provided"){
+      weighted_rmap_fn(baseArgs)
+    } else {
+      riskValidateInternalFn(baseArgs)
+    }
+    
+    df_for_roc_plot = rv$df_for_roc_plot
+    roc_plot = ggplot(df_for_roc_plot, aes(x = one_minus_specificity, y = sensitivity)) +
+      geom_step() + 
+      xlab("1 - Specificity") +
+      ylab("Sensitivity")
+    pi_summary = rv$pi_summary
+    df_for_risk_plot = as.data.frame(100 * as.matrix(pi_summary[, c("r", "pi_hat", "lower", "upper")]))
+    risk_plot = ggplot(df_for_risk_plot, aes(x = r, y = pi_hat, ymin = lower, ymax = upper)) +
+      geom_point() +
+      geom_errorbar(aes(ymin = df_for_risk_plot$lower, ymax = df_for_risk_plot$upper), width = 2) +
+      geom_abline(slope = 1, intercept = 0, color = "red", linetype = 2) +
+      xlim(0,100) + ylim(0, 100) +
+      xlab("Assigned Risk (%)") + ylab("Observed Risk (%)")
+    list(concordance_summary = rv$concordance_summary,
+         df_for_roc_plot = df_for_roc_plot,
+         roc_plot = roc_plot,
+         gof = rv$gof,
+         pi_summary = pi_summary,
+         df_for_risk_plot = df_for_risk_plot,
+         risk_plot = risk_plot)
+  }
+}
