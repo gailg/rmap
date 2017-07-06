671f1500580ced9e21a6c325091ba5127d202856
Gail Gong <gailgongster@gmail.com>
2017-07-05 23:01:06 -0700

    ChiSqFn, piHatFn, and prob_CI_Fn have been oxygenated

--- R/ChiSqFn.R
+++ R/ChiSqFn.R
@@ -1,6 +1,37 @@
-
+#' \code{ChiSqFn}
+#' 
+#' Compute the Hosmer-Lemseshow Chi-squared goodness-of-fit statistic
+#' and corresponding p-value using \code{davies}
+#' 
+#' @param baseArgs A list provided by \code{baseArgsFn}.
+#' This function calls either calls \code{gammaHatFn},
+#' \code{piHatFn} and \code{SigmaFn} or uses values
+#' calculated by them
+#' so all objects requried by either of these functions are
+#' also required by \code{ChiSqFn}.  Also required here are
+#' \code{K}, \code{N_two_stage}, and \code{rSummary}
+#' 
+#' @param extraArgs A list of named elements which can contain
+#' \code{gammaHat}, \code{lambdaHat}, \code{piHat}, and \code{Sigma}.
+#' This argument gives access to intermediate values 
+#' previously calculated.
+#' 
+#' @return A named vector holding the goodness of fit statistic
+#' and the corresponding p-value.  
+#' 
+#' @examples
+#' set.seed(1)
+#' d = df_randomSample()
+#' baseArgs = baseArgsFn(
+#'   e = d$e, t = d$t, r = d$r, tStar = 10,
+#'   design = "randomSample", riskGroup = list(K = 3),
+#'   rSummary = "median", bootstrap = FALSE)
+#' ChiSqFn(baseArgs)
+#'   
+#' @export
 
 ChiSqFn = function(baseArgs = FALSE, extraArgs = FALSE) {
+  gammaHat = if("gammaHat" %in% names(extraArgs)) extraArgs$gammaHat else gammaHatFn(baseArgs)
   piHat = if("piHat" %in% names(extraArgs)) extraArgs$piHat else piHatFn(baseArgs, extraArgs)
   Sigma = if("Sigma" %in% names(extraArgs)) extraArgs$Sigma else SigmaFn(baseArgs, extraArgs)
 
@@ -15,10 +46,8 @@ ChiSqFn = function(baseArgs = FALSE, extraArgs = FALSE) {
   ## Pearson_pval = 1 - pchisq(Pearson, baseArgs$K)
 
   sigma = sqrt(sapply(1:baseArgs$K, function(kkk) SigmaPi[kkk, kkk]) / sum(baseArgs$N_two_stage))
-  HosmerLemeshow = sum((piHat - baseArgs$rSummary)^2 / sigma^2 )
-  gammaHat = extraArgs$gammaHat
   HosmerLemeshow = sum( gammaHat * (piHat - baseArgs$rSummary)^2 / sigma^2 )
-  HL_pval =  davies(HosmerLemeshow, lambda = extraArgs$gammaHat)$Qq
+  HL_pval =  davies(HosmerLemeshow, lambda = gammaHat)$Qq
 
   ## c(Pearson = Pearson, Pearson_pval = Pearson_pval,
   ##   HosmerLemeshow = HosmerLemeshow, HL_pval = HL_pval)
--- R/prob_CI_Fn.R
+++ R/prob_CI_Fn.R
@@ -1,5 +1,22 @@
-
-
+#' Calculate the 95 percent confidence interval of a probability using a logit transformation
+#' 
+#' @param ps A numeric vector of probabilities
+#' 
+#' @param sds A numeric vector of standard deviations.  The elements
+#' in this vector correspond to the elements in \code{ps} above
+#' 
+#' @export A matrix with two columns \code{lower} and \code{upper}
+#' and a row for each element in \code{ps} and \code{sds}.
+#' 
+#' @examples 
+#' prob_CI_Fn(ps = 0.99, sds = 0.01)
+#' prob_CI_Fn(ps = c(0.98, 0.99), sds = c(0.01, 0.01))
+#' 
+#' set.seed(1)
+#' someCIs = prob_CI_Fn( ps = (1:99) / 100, sds = runif(99, 0.01, 0.05) )
+#' 
+#' @export
+#' 
 prob_CI_Fn = function(ps, sds) {
   xs = qlogis(ps)
   sd_of_xs = sds / (ps * (1 - ps))
