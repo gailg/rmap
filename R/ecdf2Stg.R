7b3465a04dd1fcb045e21bbc2aa611417b411877
Gail Gong <gailgongster@gmail.com>
2017-07-12 09:32:46 -0700

    ecdf2Stg has been oxygenated

--- R/ecdf2Stg.R
+++ R/ecdf2Stg.R
@@ -1,10 +1,41 @@
-
-## Fri Aug 12 11:28:37 PDT 2011
-## Copied from inside
-##    trunk/projects/riskModel/53-nearest-neighbor/03-scribbles/17e-destined-for-rmap-nne-mclapply-printing.R
-
-
-
+#' Weighted empirical cumulative distribution function
+#' 
+#' Compute or plot the empirical cumulative distribution function
+#' allowing for weights.  Extends ecdf
+#' 
+#' @param x A numeric vector whose cumulative distribution you
+#'   wish to compute.
+#' @param aaa A numeric vector of the same length as \code{x} 
+#'   containing the weights.
+#' @return A function of class \code{ecdf}
+#' @examples 
+#' set.seed(2)
+#' twoStageSample = df_twoStage(40)
+#' xxx = twoStageSample$d
+#' e = xxx$e
+#' t = xxx$t
+#' r = round(xxx$r, 3)
+#' tStar = 10
+#' N = twoStageSample$N
+#' design = list(N_two_stage = N, c = xxx$c)
+#' epsilon = nrow(xxx)^(-1/3)
+#' riskGroup = list(epsilon = epsilon)
+#' rSummary = "mean"
+#' bootstrap = 20
+#' confidenceLevel = 0.95
+#' multicore = FALSE
+#' verbose = TRUE
+#' baseArgs = baseArgsFn(e, t, r, tStar, design, riskGroup, rSummary, bootstrap,
+#'                       confidenceLevel, multicore, verbose)
+#' G_Fn_unweighted = ecdf(r) 
+#' 
+#' aaa = (baseArgs$N_two_stage / baseArgs$n_two_stage)[baseArgs$c]
+#' aaa
+#' G_Fn = ecdf2Stg(r, aaa)
+#' plot(G_Fn_unweighted, col = rgb(0, 0, 1, 0.7), cex = 0.5) # small blue
+#' plot(G_Fn, add = TRUE, col = rgb(1, 0, 0, 0.3))           # light red
+#' @export
+#'   
 ecdf2Stg = function (x, aaa) 
 {
     x <- sort(x)
