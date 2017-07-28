5637ac130580abf4c98282f9575904affd025dfc
Gail Gong <gailgongster@gmail.com>
2017-07-28 13:55:37 -0700

    base_args_fn translates pretty params to those expected by baseArgsFn
    rmap now calls base_args_fn, so it accepts pretty params,
    and then it calls rmap_grouped_fn.  The main results from
    the package are inside rmap_ungrouped_fn, so rmap is a
    wrapper function for rmap_ungrouped_fn.
    
    rmap_individual is the other user-friendly function of
    the package.  It supplies the indivualized attribute diagram
    only, and I've added a separate function for it so the
    user to decide if she wants to use it or not.
    
    I am thinking the user calls rmap to get rmap_answers
    and calls rmap_individual t0 get individual.
    
    I am working on three functions, currently in
    04-index-2017-07-28 called
    pretty_roc_plot(rmap_answers)
    pretty_risk_plot(rmap_answers)
    pretty_individual_risk_plot(individual)
    
    Maybe the pretty names will be changed.

--- R/rmap.R
+++ R/rmap.R
@@ -1,41 +1,7 @@
 rmap = function(e, t, r, t_star, design, risk_group, r_summary, N_bootstraps, 
                 confidence_level = 0.95, N_cores = 0, verbose = FALSE){
-  tStar = t_star
-  riskGroup = risk_group
-  rSummary = r_summary
-  bootstrap = if(N_bootstraps == 0){
-    FALSE
-  } else {
-    N_bootstraps
-  }
-  confidenceLevel = confidence_level
-  multicore = if(N_cores > 1){
-    TRUE
-  } else {
-    FALSE
-  }
-  design = if(is.character(design) && design == "random_sample"){
-    "randomSample"
-  } else if("N_first_stage" %in% names(design) && "category" %in% names(design)){
-    list(c = design$category,
-         N_two_stage = design$N_first_stage)
-  } else if("N_target" %in% names(design) && "category" %in% names(design)){
-    list(c = design$category,
-         N_target = design$N_target)
-  } else if ("target_category" %in% names(design) && "category" %in% names(design)){
-    list(c = design$category,
-         targetCategory = design$target_category)
-  } else {
-    stop(paste("Design must be 'randomSample' or a list containing 'category'",
-               "and one of 'N_first_stage', 'N_target', or 'target_category'"))
-  }
-  baseArgs = baseArgsFn(e, t, r, tStar, design, riskGroup, rSummary, bootstrap, 
-                        confidenceLevel, multicore, verbose)
-  if( baseArgs$ungrouped ) {
-    plots = rmap_ungrouped_fn(baseArgs)
-    summary = NULL
-    list(plots = plots, summary = summary)
-  } else {
-    rmap_grouped_fn(baseArgs)
-  }
+  baseArgs = base_args_fn(
+    e, t, r, t_star, design, risk_group, r_summary, N_bootstraps, 
+    confidence_level, N_cores, verbose)
+  rmap_grouped_fn(baseArgs)
 }
---
+++ R/rmap_individual.R
@@ -0,0 +1,7 @@
+rmap_individual = function(e, t, r, t_star, design, risk_group, r_summary, N_bootstraps, 
+                          confidence_level = 0.95, N_cores = 0, verbose = FALSE){
+  baseArgs = base_args_fn(
+    e, t, r, t_star, design, risk_group, r_summary, N_bootstraps, 
+    confidence_level, N_cores, verbose)
+  rmap_ungrouped_fn(baseArgs)
+}
